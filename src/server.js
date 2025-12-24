740b0583399455598a795ea80ae9d7667bd9824f
thearbaazshaikh98 <thearbaazshaikh98@gmail.com>
2025-12-24 16:22:44 +0530

    remove local config

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   await connectDB();
 
-app.listen(5000, "0.0.0.0", () => {
+  app.listen(PORT, () => {
     console.log(`Server running 🚀 on port ${PORT}`);
   });
 };
